2dae625a22dc490b176881f8c383d03291265acc
Pezhman1994 <pezhmanheidaryrostamy1994@gmail.com>
2023-11-02 02:49:15 +0330

    add new File README.MD

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,9 +1,20 @@
-package HelloWorld;
+/**
+ * This class is used to display a "Hello World" message to the console.
+ */
+package HelloWorld; // Package names should be in lowercase to conform to Java naming conventions
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world");
+public class Main {
+
+    /**
+     * The main entry point of the program.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Define a constant variable for the message
+        final String greeting = "Hello, World!";
+
+        // Print the greeting message to the console
+        System.out.println(greeting);
     }
 }
